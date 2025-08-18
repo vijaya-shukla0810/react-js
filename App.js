709dab2a20c8147a03c2a636aae8d4b6667af0e2
vijayashukla <vijaya.shukla@gdn-commerce.com>
2025-08-18 20:18:31 +0530

    basic

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement("div", {id: "parent"},
     [React.createElement("div", {id: "child"},
-        [React.createElement("h1", {}, "I am an h1 tag"), React.createElement("h2", {}, "I am an h2 tag")]
+        [React.createElement("h1", {}, "wedfuhewhfiewhsdfh😁"), React.createElement("h2", {}, "I am an h2 tag")]
      ),React.createElement("div", {id: "child2"},
         [React.createElement("h1", {}, "I am an h1 tag"), React.createElement("h2", {}, "I am an h2 tag")]
      )])
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # react js
+
+# parcel
+- Dev Build
+- Local Server
+- HMR - Hot module replacement
+- File watching algorithm - written in C++
+- Caching - faster builds
+- Minification
+- Bundling
+- Compress
+- Content hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error handling
+- Helps to host on HTTPs
+- Tree Shaking algorithm - remove unused code
+- Different dev and prod bundles
